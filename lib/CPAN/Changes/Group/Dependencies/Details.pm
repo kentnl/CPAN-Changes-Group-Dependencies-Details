cd53289020ee4ffe22e5623f89f3d96c2398b482
Kent Fredric <kentfredric@gmail.com>
2015-07-19 06:11:48 +1200

    Build results of 782ff3b (on master)

--- lib/CPAN/Changes/Group/Dependencies/Details.pm
+++ lib/CPAN/Changes/Group/Dependencies/Details.pm
@@ -4,7 +4,7 @@ use warnings;
 
 package CPAN::Changes::Group::Dependencies::Details;
 
-our $VERSION = '0.001005';
+our $VERSION = '0.001006';
 
 # ABSTRACT: Full details of dependency changes.
 
@@ -302,7 +302,7 @@ CPAN::Changes::Group::Dependencies::Details - Full details of dependency changes
 
 =head1 VERSION
 
-version 0.001005
+version 0.001006
 
 =head1 SYNOPSIS
 
